ae2df01b1e1ee436a6dfcf97f44c2a0d7f9d4800
GLAD!! <glad2121@gmail.com>
2021-10-22 00:29:22 +0900

    微修正。

--- spring-example-app/src/main/java/com/example/Application.java
+++ spring-example-app/src/main/java/com/example/Application.java
@@ -25,10 +25,10 @@ import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
-public class SpringExampleApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringExampleApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
     @Autowired
